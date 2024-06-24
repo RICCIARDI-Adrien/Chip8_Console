1a3a0fa0d4bb5843ee479e81acbe00c43246e500
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2024-06-25 00:19:02 +0200

    [Software] Fixed the keyboard source file using spaces instead of tabs. Also improved some comments.

--- Software/Sources/Keyboard.c
+++ Software/Sources/Keyboard.c
@@ -17,11 +17,11 @@ static volatile unsigned char Keyboard_Is_Menu_Key_Pressed = 0;
 /** Interrupt handler for the external interrupt 0. */
 static void __interrupt(irq(INT0), high_priority) KeyboardInterrupt(void)
 {
-    // Keep the pressed state until
-    Keyboard_Is_Menu_Key_Pressed = 1;
+	// Keep the pressed state to make it available later on
+	Keyboard_Is_Menu_Key_Pressed = 1;
 
-    // Clear the interrupt flag
-    PIR1bits.INT0IF = 0;
+	// Clear the interrupt flag
+	PIR1bits.INT0IF = 0;
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -29,34 +29,34 @@ static void __interrupt(irq(INT0), high_priority) KeyboardInterrupt(void)
 //-------------------------------------------------------------------------------------------------
 void KeyboardInitialize(void)
 {
-    // Configure the pins as digital
-    ANSELA = 0;
-    ANSELBbits.ANSELB5 = 0;
+	// Configure the pins as digital
+	ANSELA = 0;
+	ANSELBbits.ANSELB5 = 0;
 
-    // Configure the pins as inputs
-    TRISA = 0xFF;
-    TRISBbits.TRISB5 = 1;
+	// Configure the pins as inputs
+	TRISA = 0xFF;
+	TRISBbits.TRISB5 = 1;
 
-    // Assign the external interrupt 0 feature to the RB5 pin
-    INT0PPS = 0x0D; // Port B, pin 5
+	// Assign the external interrupt 0 feature to the RB5 pin
+	INT0PPS = 0x0D; // Port B, pin 5
 
-    // Configure the external interrupt 0 interrupt
-    INTCON0bits.INT0EDG = 0; // Trigger the interrupt on signal failing edge
-    PIR1bits.INT0IF = 0; // Make sure the interrupt flag is cleared to avoid triggering a false interrupt
-    PIE1bits.INT0IE = 1; // Enable the interrupt
+	// Configure the external interrupt 0 interrupt
+	INTCON0bits.INT0EDG = 0; // Trigger the interrupt on signal falling edge
+	PIR1bits.INT0IF = 0; // Make sure the interrupt flag is cleared to avoid triggering a false interrupt
+	PIE1bits.INT0IE = 1; // Enable the interrupt
 }
 
 unsigned char KeyboardReadKeysMask(void)
 {
-    return ~PORTA; // Key switches are active low
+	return ~PORTA; // Key switches are active low
 }
 
 unsigned char KeyboardIsMenuKeyPressed(void)
 {
-    unsigned char Is_Pressed = Keyboard_Is_Menu_Key_Pressed;
+	unsigned char Is_Pressed = Keyboard_Is_Menu_Key_Pressed;
 
-    // Reset the pressing state, not bothering here with disabling the key interrupt, this is a low importance feature
-    Keyboard_Is_Menu_Key_Pressed = 0;
+	// Reset the pressing state, not bothering here with disabling the key interrupt, this is a low importance feature
+	Keyboard_Is_Menu_Key_Pressed = 0;
 
-    return Is_Pressed;
+	return Is_Pressed;
 }
